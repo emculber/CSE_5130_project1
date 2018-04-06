b3657940db5cabcf1766f09045456ea6b938ef07
Viljo Wagner <vilio.wagner@continental-corporation.com>
2018-04-06 16:36:20 -0400

    work on img_processor

--- image_processing.py
+++ image_processing.py
@@ -1,16 +1,51 @@
 """
 processes the images and returns a numpy array with the most significant features
+"python.linting.pylintArgs": ["--extension-pkg-whitelist=numpy,cv2,PIL"]
 """
 
-import cv2
+import cv2 as cv
 import numpy as np
-import sys
+import PIL
 
 def process_image(path):
     """
     for now it converts the image to a numpy array
     """
-    im = cv2.imread(path)
-    print(im)
+    im = cv.imread(path)
+    gr = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 
-process_image(sys.argv[1])
+    cr = find_colors(gr)
+    cs = sorted(cr.keys(), reverse=True)
+
+    print(cr)
+    grr = reduce_color_range(gr, cs[0], cs[1])
+
+    print(find_colors(grr))
+
+    cv.imshow('grey', grr)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def reduce_color_range(ar, rpc, rdc):
+    c = np.copy(ar)
+
+    for row in range(len(c)):
+        for col in range(c[row]):
+            if c[row][col] == rpc:
+                c[row][col] = rdc
+
+    return c
+
+def find_colors(ar):
+    c = dict()
+
+    for row in ar:
+        for el in row:
+            if el in c:
+                c[el] += 1
+            else:
+                c[el] = 1
+    return c
+
+process_image('./screenshots/frame500')
+process_image('./screenshots/frame626')
